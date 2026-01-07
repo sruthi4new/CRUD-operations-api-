8e43f5e8ca3e56120bf4cb503584150e9af8576e
B Sruthi <sruthi101019@gmail.com>
2026-01-07 12:13:50 +0530

    Add files via upload
    using  mongoose

--- index.js
+++ index.js
@@ -1,168 +1,88 @@
-// import express from 'express'
-// import userData from './users.json' with {type:'json'}
-// const app = express();
-
-// app.get("/",(req,res)=>{
-//     console.log(userData);
-    
-//     res.send(userData)
-// })
-// app.get("/user/:id",(req,res)=>{
-//     const id = req.params.id;
-//     console.log(id);
-//     let filterData = userData.filter((user)=>user.id==id)
-
-//     res.send(filterData);
-    
-// })
-
-// app.get("/userName/:name",(req,res)=>{
-//     const name = req.params.name;
-//     console.log(name);
-//     let filterData = userData.filter((user)=>user.name.toLowerCase()==name.toLowerCase())
-
-//     res.send(filterData);
-    
-// })
-
-// app.listen(3200);
-
-import { log } from 'console';
+import mongoose from 'mongoose'
 import express from 'express'
-import { Agent } from 'http';
-import { MongoClient, ObjectId } from 'mongodb';
+import studentModel from './model/studentModel.js';
 
-const dbName = 'test'
-const url = "mongodb://localhost:27017"
-const client = new MongoClient(url)
+const app = express();
+//middleware to get req.body in console
+app.use(express.json())
 
+//.connect to connect mongoose with the database
+await mongoose.connect("mongodb://localhost:27017/test")
+console.log("conected.......");
 
-const app = express()
-app.use(express.urlencoded({extended:true}))
-  app.use(express.json());
+app.get("/",async (req,res)=>{
 
-app.set("view engine",'ejs')
-client.connect().then((connection)=>{
-  const db = connection.db(dbName);  
-
-//   app.get("/api",async (req,res)=>{
-//     const collection = db.collection('students')
-//     const students = await collection.find().toArray()
-//     res.send(students)
-//   })
-//    app.get("/ui",async (req,res)=>{
-//     const collection = db.collection('students')
-//     const result= await collection.find().toArray()
-//     res.render('student',{result})
-//   })
-//      app.get("/add",(req,res)=>{
-//         res.render('add-student')
-//   })
-   /* CREATE API's */
-    app.post("/api/students",async (req,res)=>{
-    const collection = db.collection('students')
-    // // console.log("collection data",collection);
-    
-    // const result = await collection.insertOne(req.body)
-    // res.redirect("/ui")
-
-    // // res.send({data:result,message:"success..."})
-    // console.log("result....",result);
-    // // return ({data:result,message:"success..."})
-    const student = {
-        name:req.body.name,
-        age:Number(req.body.age),
-        email:req.body.email
-    };
-    const result = await collection.insertOne(student);
-
-    res.status(201).json({
-        message:"student added",
-        id: result.insertedId
-    });
-    
-  })
-
-  /* UPDATE API */
-
-  app.put("/api/students/:id",async (req,res)=>{
-    const collection = db.collection('students')
-
-    const updateStudent ={
-        name:req.body.name,
-        age:Number(req.body.age),
-        email:req.body.email
-    };
-
-    const result = await collection.updateOne(
-        {_id: new ObjectId(req.params.id)},
-        {$set: updateStudent}
-    );
-
-    if(result.matchedCount == 0){
-        return res.status(404).json({
-            message:"student not found"
-        });
-    }
-    res.json({
-        message:"student updated"
-    });
-  })
-
-  /* READ API */
-  app.get("/api/students",async (req,res)=>{
-    const collection = db.collection('students')
-    const students = await collection.find().toArray();
-    res.json(students);
-  })
-
-  /* READ BY ID API */
-
-  app.get("/api/students/:id",async (req,res)=>{
-    const collection = db.collection('students');
-    const student = await collection.findOne({
-        _id: new ObjectId(req.params.id)
-    });
-
-    if(!student){
-        return res.status(404).json({
-            message:"student no found"
-        });
-    }
-    res.json(student);
-  })
-/* DELETE API */
-  app.delete("/api/students/:id",async (req,res)=>{
-    const collection = db.collection('students');
-
-    const result = await collection.deleteOne({
-        _id: new ObjectId(req.params.id)
-    });
-
-    if(result.deletedCount == 0){
-        return res.status(404).json({
-            message: "student not found"
-        });
-    }
-
-    res.json({
-        message:"student deleted "
-    });
-  })
-
+    const studentData= await studentModel.find()
+    res.send(studentData)
 })
 
-// app.set("view engine",'ejs')
+app.post("/save",async (req,res)=>{
+    console.log(req.body);
+    const {name,age,email} =req.body;
+   //validation to check the conditions
+    if(!req.body || !name || !age || !email){
+          
+    res.send({
+        message:"data not stored",
+        success:false,
+        storedInfo:null
 
-// app.get("/",async (req,res)=>{
-//         await client.connect()
-//     const db = client.db(dbName)
-//     const collection = db.collection('students')
+    })
+    return false
 
-//     const result = await collection.find().toArray()
-//     console.log(result);
-    
-//     res.render('student',{result});
-// })
+    }
+
+    const studentData = await studentModel.create(req.body)
+
+
+    res.send({
+        message:"data stored",
+        success:true,
+        storedInfo:studentData
+
+    })
+})
+
+app.put("/update/:id",async(req,res)=>{
+    const id = req.params.id;
+    console.log(req.body);
+    const studentData = await studentModel.findByIdAndUpdate(id,{
+        ...req.body
+    })
+    res.send({
+        message:"data updated",
+        success:true,
+        info:studentData
+    })
+})
+
+app.delete("/delete/:id",async(req,res)=>{
+    const id = req.params.id;
+    console.log(req.body);
+    const studentData = await studentModel.findByIdAndDelete(id,{
+        ...req.body
+    })
+    res.send({
+        message:"data deleted",
+        success:true,
+        info:studentData
+    })
+})
+
 
 app.listen(3200);
+
+
+// async function dbConnection(){
+//     await mongoose.connect("mongodb://localhost:27017/test");
+//     const schema = mongoose.Schema({
+//         name:String,
+//         age:Number,
+//         email:String
+//     })
+//     const studentsModel = mongoose.model('students',schema);
+//     const result = await studentsModel.find();
+//     console.log(result);
+    
+// }
+// dbConnection();
